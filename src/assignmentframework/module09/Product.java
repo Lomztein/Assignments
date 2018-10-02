33066e004d1bc1fb593c32fbe357777577bc6207
Lomztein <lomztein2@gmail.com>
2018-10-02 09:33:01 +0200

    It's the final countdown!

--- src/assignmentframework/module09/Product.java
+++ src/assignmentframework/module09/Product.java
@@ -11,8 +11,8 @@ package assignmentframework.module09;
  */
 public class Product {
 
-    private int id;
-    private String name;
+    private final int id;
+    private final String name;
     private double cost;
 
     public Product(int id, String name, double cost) {
